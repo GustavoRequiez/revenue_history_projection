b732aed31b05d82f7a0ebf4c04e5a72be15f9a8d
Gustavo Flores <gustavofc65@gmail.com>
2019-12-09 17:07:45 -0600

    updated to v12

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
     'author': "gflores",
     'website': "https://www.gruporequiez.com",
     'category': 'Account',
-    'version': '0.0.0.1',
+    'version': '12.0.0.0.1',
     'license': "LGPL-3",
     'depends': [
         'account',
